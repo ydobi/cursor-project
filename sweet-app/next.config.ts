6df8e2ff1d4b48ae8085ad862896b84e2b50aec2
hongkai05 <hongkai05@meituan.com>
2024-12-23 18:57:59 +0800

    feat: 优化打包

--- sweet-app/next.config.ts
+++ sweet-app/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',  // 启用静态导出
+  images: {
+    unoptimized: true  // 对于静态导出需要禁用图片优化
+  }
 };
 
 export default nextConfig;
