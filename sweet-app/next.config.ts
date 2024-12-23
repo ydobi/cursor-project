1f71cce56b8229190ac86b28d23cf18337491452
hongkai05 <hongkai05@meituan.com>
2024-12-23 20:20:36 +0800

    feat: 恢复

--- sweet-app/next.config.ts
+++ sweet-app/next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',  // 启用静态导出
-  images: {
-    unoptimized: true  // 对于静态导出需要禁用图片优化
-  },
   webpack: (config, { dev, isServer }) => {
     // 禁用缓存
     if (!dev) {
